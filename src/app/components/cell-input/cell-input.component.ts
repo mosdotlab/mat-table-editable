a9d535d76ca34b75771f9ad16d78e381c3b61d32
SergeyM <>
2025-03-19 16:15:42 +0300

    cell input select

--- src/app/components/cell-input/cell-input.component.ts
+++ src/app/components/cell-input/cell-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-cell-input',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./cell-input.component.scss']
 })
 export class CellInputComponent implements OnInit {
+	@Input() value:string;
+	@Input() type:string;
+	@Input() editMode:boolean;
 
 	constructor() { }
 
--- src/app/shared-modules/material.modules.ts
+++ src/app/shared-modules/material.modules.ts
@@ -2,14 +2,20 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { MatListModule } from '@angular/material/list';
 import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 export const material = [
 	MatToolbarModule,
 	MatIconModule,
 	MatInputModule,
 	MatSelectModule,
+	MatListModule,
 	MatTableModule,
-	MatSortModule
+	MatSortModule,
+	FormsModule,
+	MatFormFieldModule
 ];
