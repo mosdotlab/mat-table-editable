77fe9074e85d029604d7c87884fc3e00e2b1c05a
SergeyM <>
2025-03-19 16:58:27 +0300

    Status

--- src/app/models/task.models.ts
+++ src/app/models/task.models.ts
@@ -10,3 +10,22 @@ export interface IKeyValue {
 	key: number,
 	value: string,
 }
+
+export const STATUS: IKeyValue[] = [
+	{
+		key: 1,
+		value: "Создана",
+	},
+	{
+		key: 2,
+		value: "В работе",
+	},
+	{
+		key: 3,
+		value: "На тестировании",
+	},
+	{
+		key: 4,
+		value: "Завершена",
+	}
+];
