12ff8f09de4946e54a81b8f9aac878753a831fa6
SergeyM <>
2025-03-20 08:02:59 +0300

    ChangeDetectionStrategy.OnPush

--- src/app/components/cell-input/cell-input.component.ts
+++ src/app/components/cell-input/cell-input.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
 	selector: 'app-cell-input',
 	templateUrl: './cell-input.component.html',
-	styleUrls: ['./cell-input.component.scss']
+	styleUrls: ['./cell-input.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CellInputComponent {
 	@Input() label: string;
--- src/app/components/cell-select/cell-select.component.ts
+++ src/app/components/cell-select/cell-select.component.ts
@@ -1,22 +1,31 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { MatSelectChange } from '@angular/material/select';
 import { IKeyValue } from 'src/app/models/task.models';
 
 @Component({
 	selector: 'app-cell-select',
 	templateUrl: './cell-select.component.html',
-	styleUrls: ['./cell-select.component.scss']
+	styleUrls: ['./cell-select.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CellSelectComponent {
+export class CellSelectComponent implements OnChanges {
 	@Input() label: string;
 	@Input() options: IKeyValue[];
 	@Input() selectedIndex: number;
 	@Input() selectedValue: string;
 	@Input() editMode: number;
 
+	public currentValue: string;
+
 	constructor() { }
+	ngOnChanges(changes: SimpleChanges): void {
+		const val = changes?.['selectedValue']?.currentValue;
+		if (val)
+			this.currentValue = this.selectedValue;
+
+	}
 
 	public selectionChange(event: MatSelectChange) {
-		this.selectedValue = this.options.find(x => x.key === event.value)!.value;
+		this.currentValue = this.options.find(x => x.key === event.value)!.value;
 	}
 }
